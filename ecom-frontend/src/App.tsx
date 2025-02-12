9e8377c6f1dac9d4e41a25cc3ac0261b6376afa6
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-02-12 20:17:48 +0530

    Header created and styles applied

--- ecom-frontend/src/App.tsx
+++ ecom-frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/loader";
+import Header from "./components/header";
+import Login from "./pages/login";
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
@@ -29,11 +31,13 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
+      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
 
           {/* Admin Routes */}
           {/* Admin Routes */}
