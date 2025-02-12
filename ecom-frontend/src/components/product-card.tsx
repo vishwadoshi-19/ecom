7798da7c82dcfc10dcc5f224f08b6f4b75c7bf1a
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-02-12 20:48:25 +0530

    Home page built ans styled , moving on to cart page

---
+++ ecom-frontend/src/components/product-card.tsx
@@ -0,0 +1,32 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductsProps = {
+  productId: string;
+  photos: {
+    url: string;
+    public_id: string;
+  }[];
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+
+const server = "abcivgwbv";
+
+const ProductCard = ({ productId, price, name, photo, stock, handler }) => {
+  return (
+    <div className="product-card">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+      <div>
+        <button onClick={() => handler()}>
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
